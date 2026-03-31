47bfe910d07fe3e69fe3f86e61532d66c326b3e8
GyanaprakasKhandual <gyanaprakashkhnadual@gmail.com>
2026-03-31 14:31:54 +0530

    Deployed URL Chnages for cloud

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_URL || "http://localhost:8080",
+  BASE_URL: import.meta.env.VITE_API_URL || "https://toodoo-2o5c.onrender.com",
 };
 
 export const API_ENDPOINTS = {
